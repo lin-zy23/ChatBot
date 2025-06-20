2448c9446a61d971ffcf9d98e1274eff59a28ab3
lin-zy23 <179238574+lin-zy23@users.noreply.github.com>
2025-06-21 00:07:26 +0800

    Update utils.py

--- run/utils.py
+++ run/utils.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import json
-from typing import List, Tuple
+from typing import List
 
 import torch
 import torch.nn as nn
@@ -14,7 +14,7 @@ from model import ChatbotModel
 
 def load_model(model_path: str,
                tokenizer_path: str,
-               device) -> Tuple[nn.Module, TextProcessor]:
+               device) -> tuple[nn.Module, TextProcessor]:
     with open(tokenizer_path, "r", encoding="utf-8") as f:
         word_to_idx = json.load(f)
     idx_to_word = {v: k for k, v in word_to_idx.items()}
@@ -22,10 +22,10 @@ def load_model(model_path: str,
     proc = TextProcessor(word_to_idx, idx_to_word)
     model = ChatbotModel(nvoc=len(word_to_idx)).to(device)
     
-    # model = nn.DataParallel(model)
+    model = nn.DataParallel(model)
     checkpoint = torch.load(model_path, map_location=device)
     model.load_state_dict(checkpoint)
-    # model = model.module
+    model = model.module
     model.eval()
 
     return model, proc
@@ -45,12 +45,12 @@ def generate_response(model: ChatbotModel,
             seq.extend(proc.encode(h))
         inp = torch.tensor([seq], device=device)
         
-        puncts = [proc.word_to_idx[p] for p in ['，', '。', '！', '？', '<EOS>', '<UNK>']]
+        # puncts = [proc.word_to_idx[p] for p in ['，', '。', '！', '？', '<EOS>', '<UNK>']]
         
-        def apply_repetition_penalty(logits: torch.Tensor, generated: List[int]):
-            for tok in set(generated):
-                logits[tok] /= repetition_penalty
-            return logits
+        # def apply_repetition_penalty(logits: torch.Tensor, generated: List[int]):
+        #     for tok in set(generated):
+        #         logits[tok] /= repetition_penalty
+        #     return logits
 
         outs = []
         for _ in range(max_len):
@@ -58,17 +58,17 @@ def generate_response(model: ChatbotModel,
             mask = model._causal_mask(inp.size(1), device)
             
             logits = model(inp, mask)[0, -1] / temperature
-            logits = apply_repetition_penalty(logits, outs)
+            # logits = apply_repetition_penalty(logits, outs)
             probs = torch.softmax(logits, dim=-1)
             
             if not outs:
                 probs[proc.word_to_idx["<EOS>"]] = 0
                 probs = probs / probs.sum()
             
-            if outs and outs[-1] in puncts:
-                for p in puncts:
-                    probs[p] = 0
-                probs = probs / probs.sum()
+            # if outs and outs[-1] in puncts:
+            #     for p in puncts:
+            #         probs[p] = 0
+            #     probs = probs / probs.sum()
             
             sorted_probs, sorted_idx = torch.sort(probs, descending=True)
             cum_probs = torch.cumsum(sorted_probs, dim=0)
